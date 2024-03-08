be42e5a322dc582c8be074818dbea2fba01b6909
Shashwat Sahu <shashwatsahu780@gmail.com>
2024-03-08 14:06:33 +0530

    Serializers

---
+++ Serializers/app1/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializers
+from django.http import HttpResponse, JsonResponse
+# Create your views here.
+
+
+def student_details(request, pk):
+    stu = Student.objects.get(id=pk)  # stu is student ovject
+    serializer = StudentSerializers(stu)  # serializer is  Student SErializer
+    # json_data = JSONRenderer().render(serializer.data)  # serializer.data is python data
+    # json_data =json data
+    # return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(serializer.data)
+
+# Query Set - All student Data
+def student_list(request):
+    stu = Student.objects.all()
+    serializer = StudentSerializers(stu, many=True)
+    return JsonResponse(serializer.data, safe=False)
